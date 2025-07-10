2648213af7f69818b7b32dd4b53a0a7e0d469cb3
Pedro-Henrique443 <dosanjospedro66@gmail.com>
2025-07-10 10:46:38 -0300

    novidaddes adicionadas ao fitacalc

--- Model/User.php
+++ Model/User.php
@@ -47,6 +47,59 @@ class User{
             return false;
         }
     }
+
+    public function getUserByEmail($email){
+        try{
+            $sql = "SELECT * FROM user WHERE email = :email LIMIT 1";
+
+            $conn = $this->db->prepare($sql);
+
+            $conn->bindParam(':email', $email, PDO::PARAM_STR);
+
+            $conn->execute();
+            
+            return $conn->fetch(PDO::FETCH_ASSOC);
+        } catch(PDOException $error) { }
+}
+
+
+    public function getUserInfo($id, $user_fullname,$email){
+        try{
+            $sql = "SELECT user_fullname, email FROM user WERE id = :id AND user_fullname = :user_fullname AND email = :email";
+
+            $conn = $this->db->prepare($sql);
+
+            $conn->bindParam(':id', $id, PDO::PARAM_INT);
+            $conn->bindParam(':user_fullname', $user_fullname, PDO::PARAM_INT);
+            $conn->bindParam(':email', $email, PDO::PARAM_INT);
+
+            $conn->execute();
+
+            /** 
+             * fetch = querySelector();
+             * fetchAll = querySelectorAll();
+             * 
+             * FETCH_ASSOC
+             * $user{
+             * "user_fullname" => "teste",
+             * "email" => teste@example.com 
+             * }
+             * 
+             * COMO OBTER INFORMAÇÕES 
+             * $user["user_fullname"]
+             */
+
+            return $conn ->fetch(PDO::FETCH_ASSOC);
+
+        } catch(PDOException $error){
+            echo "Erro ao buscar informações do usuário " . $error->getMessage();
+            return false;
+        }
+    }
+
+
+
+
 }
 
 ?>
